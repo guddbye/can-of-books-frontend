bcb2de0003a15b68d57e2a1c4125310c2ff2cc18
Brentice Loper <54426613+guddbye@users.noreply.github.com>
2022-06-27 19:52:56 -0700

    Update BestBooks.js

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,24 +9,48 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  // Net effect is when the site loads,
+  // Data will be displayed.
+componentDidMount() {
+  this.getBooks();
 
+}
+  
+
+  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+getBooks = async () => {
+  try {
+    let results = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
+    console.log(results.data);
+    this.setState({
+      books: results.data
+    })
+  } catch(error) {
+    console.log('We have an error: ', error.response.data);
+  }
+
+}
   render() {
 
     /* TODO: render all the books in a Carousel */
-
+console.log(this.state.books);
+let books = this.state.books.map(book => ( <p>{book.name} <br/> {book.description}</p>
+))
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        <h2>Book Shelf:</h2>
 
-        {this.state.books.length ? (
+        {this.state.books.length ?
+        {books} 
+        (
           <p>Book Carousel coming soon</p>
         ) : (
-          <h3>No Books Found :(</h3>
+          <h3>No Books Entered!(</h3>
         )}
       </>
     )
   }
 }
+
 
 export default BestBooks;
